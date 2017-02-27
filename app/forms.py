612504d6c955d8ecdaf539664ed4609a0760a30f
Tuckmo <marktuckey.art@gmail.com>
2017-02-27 14:14:18 +1100

    Added email support. Updated the page to use bootstrap

--- app/forms.py
+++ app/forms.py
@@ -26,11 +26,12 @@ class EditForm(Form):
 
 class LoginForm(Form):
     openid = StringField('openid', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me',default=False)
+    remember_me = BooleanField('remember_me', default=False)
 
 
 class PostForm(Form):
-    post = StringField('post', validators=[DataRequired()])
+    post = TextAreaField('post', validators=[DataRequired()])
+
 
 class SearchForm(Form):
     search = StringField('search', validators=[DataRequired()])
